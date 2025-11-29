eb77da06895e1ce54444536c954ba5e369d9e7e1
chryszcu <jakub.chryszczanowicz@student.uwm.edu.pl>
2025-11-29 20:54:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Example as RetractingSideBar } from "./RetractingSideBar";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <RetractingSideBar />
+    </div>
+  );
+}
+
+export default App;
