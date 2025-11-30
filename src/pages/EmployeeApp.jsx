aaf19fad1a72876913fd8fcc200ec7e7f3f03410
chryszcu <jakub.chryszczanowicz@student.uwm.edu.pl>
2025-11-30 21:01:01 +0100

    dodano logowanie admina

--- src/pages/EmployeeApp.jsx
+++ src/pages/EmployeeApp.jsx
@@ -1,9 +1,20 @@
 // src/pages/EmployeeApp.jsx
+import { useState } from "react"
 import { Example as RetractingSideBar } from "../components/employee/RetractingSideBar"
+import AdminLogin from "../components/employee/Auth/AdminLogin";
+ // bo AdminLogin.jsx jest w tym samym folderze pages
 
 export default function EmployeeApp() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  // jeśli nie zalogowany → pokaż ekran logowania
+  if (!isLoggedIn) {
+    return <AdminLogin onLoginSuccess={() => setIsLoggedIn(true)} />
+  }
+
+  // jeśli zalogowany → pokaż panel admina
   return (
-    <div className="min-h-screen"> {/* Dodane h-screen i w-screen */}
+    <div className="min-h-screen">
       <RetractingSideBar />
     </div>
   )
